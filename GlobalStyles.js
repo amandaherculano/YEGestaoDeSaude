7967658e2fe74ad6f9095d0d39c8b95e57d1432f
Amanda <22.00986-8@maua.br>
2024-05-10 17:18:10 -0300

    criação com assets e fonts

---
+++ GlobalStyles.js
@@ -0,0 +1,73 @@
+/* fonts */
+export const FontFamily = {
+    interRegular: "Inter-Regular",
+    interLight: "Inter-Light",
+    interBold: "Inter-Bold",
+    interSemiBold: "Inter-SemiBold",
+    crimsonTextRegular: "CrimsonText-Regular",
+    interMedium: "Inter-Medium",
+  };
+  /* font sizes */
+  export const FontSize = {
+    size_mini: 15,
+    size_mid: 17,
+    size_xs: 12,
+    size_xl: 20,
+    size_mid_1: 17,
+    size_15xl_3: 34,
+    size_3xs: 10,
+    size_5xl: 24,
+    size_lg: 18,
+    size_sm: 14,
+    size_5xs: 8,
+    size_smi: 13,
+    size_base: 16,
+    size_3xl: 22,
+    size_11xl: 30,
+    size_6xl: 25,
+  };
+  /* Colors */
+  export const Color = {
+    colorWhite: "#fff",
+    colorDarkseagreen_100: "#61a186",
+    colorDarkseagreen_200: "rgba(97, 161, 134, 0.4)",
+    colorDarkgray_100: "#9f9d9d",
+    colorDarkgray_200: "rgba(159, 157, 157, 0.5)",
+    colorDarkgray_300: "rgba(159, 157, 157, 0.66)",
+    colorGray_100: "#929292",
+    colorGray_200: "#8e8e8e",
+    colorGray_300: "rgba(0, 0, 0, 0.5)",
+    colorGray_400: "rgba(0, 0, 0, 0.68)",
+    colorGray_500: "rgba(0, 0, 0, 0.35)",
+    colorGray_600: "rgba(255, 255, 255, 0.71)",
+    colorGray_700: "rgba(0, 0, 0, 0.42)",
+    colorGray_800: "rgba(0, 0, 0, 0.6)",
+    colorGray_900: "rgba(0, 0, 0, 0.4)",
+    colorGray_1000: "rgba(142, 142, 142, 0.3)",
+    colorGray_1100: "rgba(0, 0, 0, 0.3)",
+    colorDarkslategray_100: "#454545",
+    colorDarkslategray_200: "#444",
+    colorDarkslategray_300: "#3c3c3c",
+    colorDarkolivegreen: "#627254",
+    colorCadetblue_100: "#749488",
+    colorCadetblue_200: "#729486",
+    colorCadetblue_300: "#5e9a81",
+    colorGainsboro_100: "#e6e4e4",
+    colorGainsboro_200: "#d9d9d9",
+    colorBlack: "#000",
+    colorBlueviolet: "#9747ff",
+    colorRed_100: "#da0000",
+  };
+  /* Paddings */
+  export const Padding = {
+    p_9xs: 4,
+  };
+  /* border radiuses */
+  export const Border = {
+    br_3xs: 10,
+    br_10xs: 3,
+    br_8xs: 5,
+    br_11xl: 30,
+    br_mini: 15,
+  };
+  
